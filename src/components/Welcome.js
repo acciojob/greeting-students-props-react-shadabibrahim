09aeada91a3db95bc93b238fb83f29b328632272
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-02-16 00:17:43 +0530

    submitting code-Fri Feb 16 2024 00:17:42 GMT+0530 (India Standard Time)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+const Welcome = ({ name }) => {
+    return (<div>
+        <h1>"Hey !{name}"</h1>
+        <h2>"Welcome to School."</h2>
+        <p>"Welcome to School."</p>
+    </div>);
+
+
+}
+export default Welcome;
